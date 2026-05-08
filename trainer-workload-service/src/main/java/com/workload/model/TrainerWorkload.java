9a1b341412adb0515ea9f9dd47056638c5787555
nikusha09 <nikusha.bakhtadze123@gmail.com>
2026-05-09 02:14:10 +0400

    replaced in-memory storage with MongoDB for trainer workload tracking

--- trainer-workload-service/src/main/java/com/workload/model/TrainerWorkload.java
+++ trainer-workload-service/src/main/java/com/workload/model/TrainerWorkload.java
@@ -3,17 +3,41 @@ package com.workload.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(collection = "trainer_workload")
+@CompoundIndex(name = "first_last_name_idx", def = "{'firstName': 1, 'lastName': 1}")
 public class TrainerWorkload {
+
+    @Id
+    private String id;
     private String username;
     private String firstName;
     private String lastName;
-    private boolean isActive;
-    private Map<Integer, Map<Integer, Integer>> yearMonthDuration = new HashMap<>();
+    private boolean active;
+    private List<YearSummary> years = new ArrayList<>();
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class YearSummary {
+        private int year;
+        private List<MonthSummary> months = new ArrayList<>();
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MonthSummary {
+        private int month;
+        private int trainingsSummaryDuration;
+    }
 }
